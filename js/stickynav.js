7bef06ce2b80a5864dcb706ff2adbf1f5809af79
Eunice Kim <keunjoo@berkeley.edu>
2016-09-06 20:21:54 -0700

    Delete stickynav js file

--- js/stickynav.js
+++
@@ -1,8 +0,0 @@
-var num = 0; //number of pixels before modifying styles
-$(window).bind('scroll', function () {
-    if ($(window).scrollTop() > num) {
-        $('.nav-right').addClass('fixed');
-    } else {
-        $('.nav-right').removeClass('fixed');
-    }
-});
